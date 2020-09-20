3e21992040c9203994fd9ca34a4d6874285e7080
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2020-09-20 18:34:48 -0400

    Finished building the composite view and Tutorial Part 1

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -11,22 +11,30 @@ struct ContentView: View {
     var body: some View {
         // VStack holds a vertical stack of elements
         // optional parameters, like alignment
-        VStack(alignment: .leading){
-         
-            // Text element that accepts a string
-            Text("Turtle Rock")
-                .font(.title)
-                .foregroundColor(.green)
-            
-            // Stacks can be embedded
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading){
+             
+                // Text element that accepts a string
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                // Stacks can be embedded
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                }
             }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
