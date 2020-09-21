2d06075041056dec25c1dce828721e2cb953f20f
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2020-09-21 12:46:45 -0400

    Added Part 2 downloaded from apple. No changes so far

---
+++ Part 1/SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Kenneth Carroll on 9/20/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LandmarkDetail()
+        }
+    }
+}
---
+++ Part 1/SwiftUIDemo/Views/LandmarkDetail.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Kenneth Carroll on 9/20/20.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        // VStack holds a vertical stack of elements
+        // optional parameters, like alignment
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading){
+             
+                // Text element that accepts a string
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                // Stacks can be embedded
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                }
+            }
+            .padding()
+            Spacer()
+        }
+    }
+}
+
+struct LadnmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
